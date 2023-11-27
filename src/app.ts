e9692fb2e715b590a62ceec7afc7bb8d02467c46
pm-rahman <p.m.rahaman0181@gmail.comm>
2023-11-27 20:12:50 +0600

    eslint setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import { userRouter } from './app/module/user/user.router';
 const app = express();
 
 app.use(cors());
 app.use(express.json())
+
+app.use('/api/v1/users',userRouter)
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
